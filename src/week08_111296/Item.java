4515f7eea92e3c5f3720bcd1110739c2dd32217c
SourEminaya <nyekocharmu@gmail.com>
2025-10-29 11:22:44 +0700

    Item Class

---
+++ src/week08_111296/Item.java
@@ -0,0 +1,40 @@
+package week08_111296;
+
+public class Item {
+	private String Name;
+	private String Type;
+	private int Price;
+	
+	public Item(String Name, String Type, int Price) {
+		this.Name=Name;
+		this.Type=Type;
+		this.Price=Price;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getType() {
+		return Type;
+	}
+
+	public void setType(String type) {
+		Type = type;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public void setPrice(int price) {
+		Price = price;
+	}
+	
+	
+
+}
